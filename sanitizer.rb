1b4f933efe6eace1c85c55ceb912c7473b5672de
liangwei <xue.409@osu.edu>
2020-10-04 12:39:02 -0400

    Grouping locations and remove duplicates

--- sanitizer.rb
+++ sanitizer.rb
@@ -1,4 +1,15 @@
-
+load 'item.rb'
 class Sanitizer
+  def groupLocation(items)
+      groupArray = items.group_by(&location);
+      return groupArray;
+  end
 
+  def uniqueDuplicate(groupArray)
+    groupArray.length.time do |i|
+      uniqueDuplicate += group.index[i].unique(&name);
+    end
+    return uniqueDuplictae;
+  end
+  return uniqueDuplicate;
 end

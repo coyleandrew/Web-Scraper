4d9fcbb90cf05721b0a92836ead8cb122b53395d
mdettm9 <dettmer.16@buckeyemail.osu.edu>
2020-10-05 12:11:02 -0400

    All of the addresses should be working now with the lat/long functionality. Also made some changes to the sanitizer - it wasn't working before.

--- sanitizer.rb
+++ sanitizer.rb
@@ -1,18 +1,17 @@
 load 'item.rb'
 class Sanitizer
   def groupLocation(items)
-      groupArray = items.group_by(&location);
-      return groupArray;
+      groupArray = items.group_by(&location)
+      return groupArray
   end
 
   def uniqueDuplicate(groupArray)
     groupArray.length.time do |i|
-      uniqueDuplicate = groupArray[i].values;
+      uniqueDuplicate = groupArray[i].values
       uniqueDuplicate.length.time do|k|
         uniqueDuplicate.unique(&name);
       end
+      return uniqueDuplicate
     end
-    return uniqueDuplictae;
   end
-  return uniqueDuplicate;
 end
